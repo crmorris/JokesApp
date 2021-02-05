631ea19b2168248d2f98f6c4905e4eb418035c51
crmorris <33641568+crmorris@users.noreply.github.com>
2021-02-04 18:42:04 -0600

    initial commit

---
+++ JokesWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApplication.Models;
+
+namespace JokesWebApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebApplication.Models.Joke> Joke { get; set; }
+    }
+}
